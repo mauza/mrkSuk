57ac17c792c5e7709e9fa6812c6b84a22f45e9ef
BlueberryBits <chasenaisbitt@gmail.com>
2018-04-27 00:55:52 -0400

    more bits

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
 import tensorflow as tf 
 hello = tf.constant('Hello, TensorFlow!')
 sess = tf.Session()
-print(sess.run(hello + ' eat a dick'))
+print(sess.run(hello))
--- mrkSuk.py
+++ mrkSuk.py
@@ -1,7 +1,17 @@
-import urllib2
+import urllib.request as urllib2
 from bs4 import BeautifulSoup
 
 snp500 = 'https://en.wikipedia.org/wiki/List_of_S%26P_500_companies'
 
 page = urllib2.urlopen(snp500)
 soup = BeautifulSoup(page, 'html.parser')
+
+data = []
+table = soup.find('table', attrs={'class':'wikitable'})
+
+rows = table.find_all('tr')
+for row in rows:
+    cols = row.find_all('td')
+    cols = [ele.text.strip() for ele in cols]
+    data.append([ele for ele in cols if ele])
+print(data)
